322d3dabb8120aa58849e8ba58889a0b357ad478
Juho Naalisvaara <juho.naalisvaara@cs.helsinki.fi>
2009-09-28 21:29:49 +0300

    Lisätty henkilön lisääminen

--- src/main/java/addressbook/AddressBook.java
+++ src/main/java/addressbook/AddressBook.java
@@ -1,13 +1,72 @@
 package addressbook;
 
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class AddressBook {
 	
+	Vector<Person> persons;
+
 	public AddressBook() {
-	
+		this.persons = new Vector<Person>();
 	}
 	
 	public String toString() {
-		return "";
+		StringBuilder sb = new StringBuilder();
+		
+		Enumeration<Person> enumeration = this.persons.elements();
+		while(enumeration.hasMoreElements()) {
+			Person p = enumeration.nextElement();
+			sb.append(p.getLastName());
+			sb.append(", ");
+			sb.append(p.getFirstName());
+			sb.append(", ");
+			sb.append(p.getAddress());
+			sb.append(", ");
+			sb.append(p.getPhoneNumber());
+			sb.append("\n");			
+		}
+		return sb.toString();
+	}
+
+	public int numberOfPersons() {
+		return this.persons.size();
+	}
+
+	public void addPerson(String lastName, String firstName, String address, String phoneNumber) {
+			this.persons.add(new Person(lastName, firstName, address, phoneNumber));		
+	}
+
+}
+
+class Person {
+
+	private final String lastName;
+	private final String firstName;
+	private final String address;
+	private final String phoneNumber;
+
+	public Person(String lastName, String firstName, String address, String phoneNumber) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
 	}
 
 }
--- src/test/java/addressbook/AdressBookTest.java
+++ src/test/java/addressbook/AdressBookTest.java
@@ -20,9 +20,31 @@ public class AdressBookTest {
         public void test_Is_empty() {
             assertEquals("", book.toString());
         }    	
+        
+        public void test_has_no_persons() {
+        	assertEquals(0, book.numberOfPersons());
+        }
     }
     
     // TODO: Osoitekirjaan on toteutettu TDD:llä henkilöiden lisääminen. Kaikki testit menevät läpi.
-    // TODO: Osoitekirjaan on toteutettu TDD:llä henkilöiden lisääminen ja hakeminen. Kaikki testit menevät läpi. 
+    public static class When_a_new_person_is_added_to_addressbook extends TestCase {
+    	private AddressBook book;
+    	
+    	protected void setUp() throws Exception {
+    		book = new AddressBook();
+    	}
+    	
+    	public void test_add_a_new_person() {
+    		book.addPerson("Kokkonen", "Pirjo", "D239", "(09) 1951 1123");
+    		
+    		assertEquals(1, book.numberOfPersons());
+    		assertEquals("Kokkonen, Pirjo, D239, (09) 1951 1123\n",
+    					 book.toString());
+    	}
+    	
+    }
+    
+    // TODO: Osoitekirjaan on toteutettu TDD:llä henkilöiden lisääminen ja hakeminen. 
+    // Kaikki testit menevät läpi. 
 
 }
